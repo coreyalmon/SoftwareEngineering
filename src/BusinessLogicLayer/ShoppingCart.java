d85f68a98aff08420c8efe5fe27f474d1c1a1330
coreyalmon <59987216+coreyalmon@users.noreply.github.com>
2022-08-03 23:42:06 -0400

    Update ShoppingCart.java

--- src/BusinessLogicLayer/ShoppingCart.java
+++ src/BusinessLogicLayer/ShoppingCart.java
@@ -1,9 +1,11 @@
 package BusinessLogicLayer;
 
+import java.util.Iterator;
+
 import DataAccessLayer.FoodType;
 
-// Will soon be iterable (be able to use for each loops)
-public class ShoppingCart implements Cart {
+// Will soon be iterable (be able to use for each loops). 
+public class ShoppingCart implements Cart, Iterable<Integer> {
 	
 	private int[] cart;
 	private int numberOfItems;
@@ -33,5 +35,32 @@ public class ShoppingCart implements Cart {
 	
 	public int get(FoodType FOOD_TYPE) {
 		return cart[FOOD_TYPE.ordinal()];
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		// TODO Auto-generated method stub
+		return new CartIterator();
+	}
+	
+	private class CartIterator implements Iterator<Integer> {
+
+		private int foodTypeOrdinal;
+		
+		
+		public CartIterator() {
+			foodTypeOrdinal = 0;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return foodTypeOrdinal < FoodType.values().length;
+		}
+
+		@Override
+		public Integer next() {
+			return cart[foodTypeOrdinal++];
+		}
+		
 	}
 }
