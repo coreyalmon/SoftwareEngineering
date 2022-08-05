6a94c1ca2604f321ce6d3dbc6bea828e1b73ae45
Jingshi Liu <jingshiliu07@gmail.com>
2022-08-05 02:13:30 -0400

    Update ConditionalCoupon.java

--- src/BusinessLogicLayer/ConditionalCoupon.java
+++ src/BusinessLogicLayer/ConditionalCoupon.java
@@ -1,17 +1,25 @@
+package BusinessLogicLayer;
+
 public class ConditionalCoupon extends FixedAmountCoupon{
     private int minTotal;
 
-    ConditionalCoupon(String description, int fixedDeduct, int minTotal,  ShoppingLogic shoppingLogic){
-        super(description, fixedDeduct, shoppingLogic);
+    ConditionalCoupon(String description, int fixedDeduct, int minTotal,  ShoppingLogic shoppingLogic, COUPON_TYPE coupon_type){
+        super(description, fixedDeduct, shoppingLogic, coupon_type);
         this.minTotal = minTotal;
     }
 
     @Override
     public void cutPrice() {
         var shoppingTracker = super.getShoppingTracker();
-        double sum = shoppingTracker.getSum();
-        if(! isUsed && minTotal < sum){
-            shoppingTracker.setSum(sum - super.getFixedDeduct());
+        double sum = shoppingTracker.getTotalCostOfItems();
+        if(minTotal < sum){
+            shoppingTracker.setTotalCostOfItems(sum - super.getFixedDeduct());
         }
     }
+
+    @Override
+    public COUPON_TYPE getCouponType() {
+        return super.getCouponType();
+    }
 }
+
